961cab06f09bc59c87b84e0bc2e0e6191f4bf66c
Daniel Toledo <dan992965@gmail.com>
2023-08-25 17:43:56 -0300

    feature: Adiciona dados simulando o db.

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-from flask import Flask
+from flask import Flask, make_response, jsonify, request
+from bd import Carros
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
+@app.route('/carros', methods=['GET'])
+def get_carros():
+    return make_response(
+        jsonify(
+            message = 'Lista de carros',
+            data = Carros
+        )
+    )
+
+@app.route('/carros', methods=['POST'])
+def create_carro():
+    carro = request.json
+    Carros.append(carro)
+    return make_response(
+        jsonify(
+            message = 'Carro cadastrado com sucesso.',
+            data = carro
+        )
+    )
 
 app.run()
