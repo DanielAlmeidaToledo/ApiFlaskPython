28415545942edfe386cd11d485a48a7ad11c29a4
Daniel Toledo <dan992965@gmail.com>
2023-08-25 17:43:56 -0300

    feature: Adiciona dados simulando o db.

---
+++ bd.py
@@ -0,0 +1,32 @@
+Carros = [
+    {
+        'id': 1,
+        'marca': 'Ford',
+        'modelo': 'Fiesta',
+        'ano': 2019,
+    },
+    {
+        'id': 2,
+        'marca': 'Fiat',
+        'modelo': 'Uno',
+        'ano': 2018,
+    },
+    {
+        'id': 3,
+        'marca': 'Chevrolet',
+        'modelo': 'Camaro',
+        'ano': 2017,
+    },
+    {
+        'id': 4,
+        'marca': 'Volkswagen',
+        'modelo': 'Gol',
+        'ano': 2016,
+    },
+    {
+        'id': 5,
+        'marca': 'Renault',
+        'modelo': 'Sandero',
+        'ano': 2015,
+    }
+]
